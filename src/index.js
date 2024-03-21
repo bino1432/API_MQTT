b6ffa05bde1479a95bf5470bbb5d84677a0e742b
Bernardo P Zapelini <94933673+bino1432@users.noreply.github.com>
2024-03-21 20:11:48 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const server = express();
+
+server.get("/", (req,res) => {
+    return res.json({mensagem: "asdads"})
+})
+
+server.listen(3000, () => {
+    console.log("Servidor online")
+})
