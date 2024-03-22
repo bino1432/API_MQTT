f43e6ee9059f9df37dd1789f361bb5e6fcf6cb2c
bino1432 <bernardozplz@gmail.com>
2024-03-21 21:32:30 -0300

    API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,62 @@
 const express = require("express")
 const server = express();
 
+const mqtt = require('mqtt');
+
+let mqttClient;
+
+function connectToBroker() {
+  const clientId = "bino1432";
+
+  // Change this to point to your MQTT broker
+  const hostURL = `mqtt://broker.mqttdashboard.com:1883`;
+
+  // const options = {
+  //   keepalive: 60,
+  //   clientId: clientId,
+  //   protocolId: "MQTT",
+  //   protocolVersion: 4,
+  //   clean: true,
+  //   reconnectPeriod: 1000,
+  //   connectTimeout: 30 * 1000,
+  // };
+
+  mqttClient = mqtt.connect(hostURL);
+
+  mqttClient.on("error", (err) => {
+    console.log("Error: ", err);
+    mqttClient.end();
+  });
+
+  mqttClient.on("reconnect", () => {
+    console.log("Reconnecting...");
+  });
+
+  mqttClient.on("connect", () => {
+    console.log("Client connected:" + clientId);
+  });
+
+  // Received Message
+  mqttClient.on("message", (topic, message) => {
+    console.log(
+      "Received Message: " + message.toString() + "\nOn topic: " + topic
+    );
+  });
+}
+ 
+function subscribeToTopic(topic) {
+  console.log(`Subscribing to Topic: ${topic}`);
+
+  mqttClient.subscribe(topic);
+}
+
+connectToBroker();
+subscribeToTopic("led/1");
+subscribeToTopic("led/2");
+subscribeToTopic("led/3");
+subscribeToTopic("temp/def");
+subscribeToTopic("temp/atl");
+
 server.get("/", (req,res) => {
     return res.json({mensagem: "API funcionado"})
 })
